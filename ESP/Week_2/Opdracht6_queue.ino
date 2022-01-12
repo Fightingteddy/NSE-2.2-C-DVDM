99309ea8381a225322b9331f2d52d476147722ef
Fightingteddy <95029100+Fightingteddy@users.noreply.github.com>
2022-01-12 16:09:51 +0100

    Update Opdracht6_queue.ino

--- ESP/Week_2/Opdracht6_queue.ino
+++ ESP/Week_2/Opdracht6_queue.ino
@@ -112,11 +112,11 @@ state &= ~(1 << -event);
 if ( state == enable ) { //beide knopjes ingedrukt
 digitalWrite(GPIO_LED,HIGH); //ledje gaat aan
 
-if( xQueueSendToBack(queuehandle, printLocalTime(), 0) != pdPASS)  //queue is vol
-{
-  xQueueReceive(queuehandle, &timestamp, 0); //voorste waarde wordt gelezen, in &timestamp gezet en verwijderd.
-  printf("Time trown away %s\r\n" , timestamp); //print voorste waarde van hierboven uit
-} 
+    if(xQueueSendToBack(queuehandle, printLocalTime(), 0) != pdPASS)  //queue is vol
+      {
+        xQueueReceive(queuehandle, &timestamp, 0); //voorste waarde wordt gelezen, in &timestamp gezet en verwijderd.
+        printf("Time trown away %s\r\n" , timestamp); //print voorste waarde van hierboven uit
+      } 
 else { //led gaat uit als een knopje niet meer wordt ingedrukt
 // Deactivate press
 digitalWrite(GPIO_LED,LOW); //ledje gaat uit
